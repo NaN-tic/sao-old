93b16f32af4d8afcccaf11723547c5020a0118b2
C?dric Krier <ced@b2ck.com>
2016-10-20 16:47:21 +0200

    Avoid calling unnecessary domain_parser
    issue5918
    review31581002

--- src/screen.js
+++ src/screen.js
@@ -257,19 +257,21 @@
         },
         bookmark_match: function() {
             var current_text = this.get_text();
-            var current_domain = this.screen.domain_parser().parse(current_text);
-            this.but_star.prop('disabled', !current_text);
-            var star = this.get_star();
-            var bookmarks = this.bookmarks();
-            for (var i=0; i < bookmarks.length; i++) {
-                var id = bookmarks[i][0];
-                var name = bookmarks[i][1];
-                var domain = bookmarks[i][2];
-                var text = this.screen.domain_parser().string(domain);
-                if ((text === current_text) ||
-                        (Sao.common.compare(domain, current_domain))) {
-                    this.set_star(true);
-                    return id;
+            if (current_text) {
+                var current_domain = this.screen.domain_parser().parse(current_text);
+                this.but_star.prop('disabled', !current_text);
+                var star = this.get_star();
+                var bookmarks = this.bookmarks();
+                for (var i=0; i < bookmarks.length; i++) {
+                    var id = bookmarks[i][0];
+                    var name = bookmarks[i][1];
+                    var domain = bookmarks[i][2];
+                    var text = this.screen.domain_parser().string(domain);
+                    if ((text === current_text) ||
+                            (Sao.common.compare(domain, current_domain))) {
+                        this.set_star(true);
+                        return id;
+                    }
                 }
             }
             this.set_star(false);
